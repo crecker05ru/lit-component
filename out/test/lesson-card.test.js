e7c981c3862f89031f18583b7844767bf08972ec
crecker05ru <fort2652@rambler.ru>
2022-04-29 14:44:41 +0300

    test added

---
+++ out/test/lesson-card.test.js
@@ -0,0 +1,57 @@
+import { html, fixture, expect } from '@open-wc/testing';
+import '../src/lesson-card';
+describe('LessonCard properties', () => {
+    it('has colums', async () => {
+        const el = await fixture(html `<lesson-card></lesson-card>`);
+        expect(el.colums).to.equal(8);
+    });
+    it('has price ', async () => {
+        const el = await fixture(html `<lesson-card></lesson-card>`);
+        expect(el.price).to.equal(5);
+    });
+    it('has lessons count ', async () => {
+        const el = await fixture(html `<lesson-card></lesson-card>`);
+        expect(el.lessonsCount).to.equal(4);
+    });
+    it('has lessons time  ', async () => {
+        const el = await fixture(html `<lesson-card></lesson-card>`);
+        expect(el.lessonsTime).to.equal(50);
+    });
+    it('has rating', async () => {
+        const el = await fixture(html `<lesson-card></lesson-card>`);
+        expect(el.rating).to.equal(4);
+    });
+    it('has rating', async () => {
+        const el = await fixture(html `<lesson-card></lesson-card>`);
+        expect(el.rating).to.equal(4);
+    });
+});
+describe("LossonCard html", () => {
+    it('element accessible', async () => {
+        const el = await fixture(html `<lesson-card></lesson-card>`);
+        await expect(el).shadowDom.to.be.accessible();
+    });
+});
+describe("LossonCard override attribute", () => {
+    it('colums', async () => {
+        const el = await fixture(html `<lesson-card colums="5"></lesson-card>`);
+        expect(el.colums).to.equal(5);
+    });
+    it('lesson count', async () => {
+        const el = await fixture(html `<lesson-card lessonsCount="9"></lesson-card>`);
+        expect(el.lessonsCount).to.equal(9);
+    });
+    it('lesson time', async () => {
+        const el = await fixture(html `<lesson-card lessonsTime="170"></lesson-card>`);
+        expect(el.lessonsTime).to.equal(170);
+    });
+    it('rating', async () => {
+        const el = await fixture(html `<lesson-card rating="3"></lesson-card>`);
+        expect(el.rating).to.equal(3);
+    });
+    it('slot', async () => {
+        const el = await fixture(html `<lesson-card >Children elem</lesson-card>`);
+        expect(el.innerHTML).to.equal("Children elem");
+    });
+});
+//# sourceMappingURL=lesson-card.test.js.map
